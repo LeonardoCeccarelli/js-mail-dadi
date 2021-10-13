1535b53145c472817cf23abb78dbb723927131ba
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-13 17:52:07 +0200

    add form in ex-mail

--- ex_email/mail.js
+++ ex_email/mail.js
@@ -1,25 +1,38 @@
 // Chiedo all'utente di inserire una mail
-const mailUtente = prompt("Per accedere, inserisci la tua e-mal")
+// const mailUtente = prompt("Per accedere, inserisci la tua e-mal")
 
-// Creo array contente email già esistenti
-const listaMailUtenti = ["mario.rossi@gmail.com", "giuseppe.verdi@gmail.com", "luigi.bianchi@gmail.com", "maria.ferrari@gmail.com"]
+// Creo barra inserimento mail per utente
+const mailElemento = document.querySelector("[name='mail']")
 
-let mailEsistente = false
+// Creo bottone per validazione e aggiungo un evento al click
+const btnSubmit = document.getElementById("btn_submit")
 
-// Creo il ciclo che mi permette di verificare se l'email inserita dall'utente è già esistente oppure no
-for (i = 0; i < listaMailUtenti.length; i++) {
-    const mailCorrente = listaMailUtenti[i]
+btnSubmit.addEventListener("click", function () {
 
-    // Creo la condizione che mi permette di verificare """"""""""""
-    // Se esiste cambio variabile (true), altrimenti no (false)
-    if (mailUtente.toLowerCase() === mailCorrente.toLowerCase()) {
-        mailEsistente = true
+    // Ricevo dati dall'utente
+    const mailUtente = mailElemento.value
+
+    // Creo array contente email già esistenti
+    const listaMailUtenti = ["mario.rossi@gmail.com", "giuseppe.verdi@gmail.com", "luigi.bianchi@gmail.com", "maria.ferrari@gmail.com"]
+
+    let mailEsistente = false
+
+    // Creo il ciclo che mi permette di verificare se l'email inserita dall'utente è già esistente oppure no
+    for (i = 0; i < listaMailUtenti.length; i++) {
+        const mailCorrente = listaMailUtenti[i]
+
+        // Creo la condizione che mi permette di verificare """"""""""""
+        // Se esiste cambio variabile (true), altrimenti no (false)
+        if (mailUtente.toLowerCase() === mailCorrente.toLowerCase()) {
+            mailEsistente = true
+        }
     }
-}
 
-// Verifico email tramite variabile inserita precedentemente e invio i corrispettivi alert
-if (!mailEsistente) {
-    alert("Accesso negato, e-mail non esistente. Si prega di effettura la registrazione.")
-} else {
-    alert("E-mail trovata. Accesso eseguito con successo.")
-}
+    // Verifico email tramite variabile inserita precedentemente e invio i corrispettivi alert
+    if (!mailEsistente) {
+        alert("Accesso negato, e-mail non esistente. Si prega di effettura la registrazione.")
+    } else {
+        alert("E-mail trovata. Accesso eseguito con successo.")
+    }
+
+})
